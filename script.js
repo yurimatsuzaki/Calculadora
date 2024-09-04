65a4c4c5343e1dc206b6402024705eab916c619b
yurimatsuzaki <155598762+yurimatsuzaki@users.noreply.github.com>
2024-09-04 16:55:20 -0300

    iniciando a dinâmica

---
+++ script.js
@@ -0,0 +1,52 @@
+let visor = document.getElementById('visor')
+
+function Clicar1(){
+    var um = document.getElementById('um')
+    visor.innerHTML+='1'
+}
+function Clicar2(){
+    var dois = document.getElementById('dois')
+    visor.innerHTML+='2'
+    
+}
+function Clicar3(){
+    var tres = document.getElementById('tres')
+    visor.innerHTML+='3'
+    
+}
+function Clicar4(){
+    var quatro = document.getElementById('quatro')
+    visor.innerHTML+='4'
+    
+}
+function Clicar5(){
+    var cinco = document.getElementById('cinco')
+    visor.innerHTML+='5'
+    
+}
+function Clicar6(){
+    var seis = document.getElementById('seis')
+    visor.innerHTML+='6'
+    
+}
+function Clicar7(){
+    var sete = document.getElementById('sete')
+    visor.innerHTML+='7'
+    
+}
+function Clicar8(){
+    var oito = document.getElementById('oito')
+    visor.innerHTML+='8'
+    
+}
+function Clicar9(){
+    var nove = document.getElementById('nove')
+    visor.innerHTML+='9'
+
+}
+
+function ClicarLimpar(){
+    var limpar = document.getElementById('limpar')
+    visor.innerHTML=' '
+}
+
