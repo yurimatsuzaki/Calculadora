792b7ed50bb897f6150e1d70d70cb6be4560816f
yurimatsuzaki <155598762+yurimatsuzaki@users.noreply.github.com>
2024-09-10 23:01:56 -0300

    dando continuidade a obra

--- script.js
+++ script.js
@@ -5,51 +5,56 @@ let num2
 function Clicar1(){
     var um = document.getElementById('um')
     visor.innerHTML+='1'
+    num1 += 1
 }
+
 function Clicar2(){
     var dois = document.getElementById('dois')
     visor.innerHTML+='2'
-    
 }
+
 function Clicar3(){
     var tres = document.getElementById('tres')
     visor.innerHTML+='3'
-    
 }
+
 function Clicar4(){
     var quatro = document.getElementById('quatro')
     visor.innerHTML+='4'
-    
 }
+
 function Clicar5(){
     var cinco = document.getElementById('cinco')
     visor.innerHTML+='5'
-    
 }
+
 function Clicar6(){
     var seis = document.getElementById('seis')
     visor.innerHTML+='6'
-    
 }
+
 function Clicar7(){
     var sete = document.getElementById('sete')
     visor.innerHTML+='7'
-    
 }
+
 function Clicar8(){
     var oito = document.getElementById('oito')
     visor.innerHTML+='8'
-    
 }
+
 function Clicar9(){
     var nove = document.getElementById('nove')
     visor.innerHTML+='9'
+}
 
+function ClicarZero(){
+    var zero = document.getElementById('zero')
+    visor.innerHTML+='0'
 }
 
 function ClicarApagar(){
     var nove = document.getElementById('apagar')
-    visor.innerHTML -= ''
 }
 
 function ClicarLimpar(){
