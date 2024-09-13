060d38dc1fe7f882f74da19f525f9ddf6dc886d1
yurimatsuzaki <155598762+yurimatsuzaki@users.noreply.github.com>
2024-09-13 12:13:06 -0300

    criando a logica de operações

--- script.js
+++ script.js
@@ -1,64 +1,207 @@
 let visor = document.getElementById('visor')
-let num1
-let num2
+let num1 = 0
+let num2 = 0
+let num3 = 0
+let verificador = 0
 
 function Clicar1(){
-    var um = document.getElementById('um')
     visor.value += 1
 }
 
 function Clicar2(){
-    var dois = document.getElementById('dois')
     visor.value += 2
 }
 
 function Clicar3(){
-    var tres = document.getElementById('tres')
     visor.value += 3
 }
 
 function Clicar4(){
-    var quatro = document.getElementById('quatro')
     visor.value += 4
 }
 
 function Clicar5(){
-    var cinco = document.getElementById('cinco')
     visor.value += 5
 }
 
 function Clicar6(){
-    var seis = document.getElementById('seis')
     visor.value += 6
 }
 
 function Clicar7(){
-    var sete = document.getElementById('sete')
     visor.value += 7
 }
 
 function Clicar8(){
-    var oito = document.getElementById('oito')
     visor.value += 8
 }
 
 function Clicar9(){
-    var nove = document.getElementById('nove')
     visor.value += 9
 }
 
 function ClicarZero(){
-    var zero = document.getElementById('zero')
     visor.value += 0
 }
 
 function ClicarLimpar(){
-    var limpar = document.getElementById('limpar')
     visor.value = ' '
+    num1 = 0
+    num2 = 0
+    num3 = 0
+    res = 0
 }
 
 function ClicarMais(){
-    var somar = document.getElementById('mais')
+    if(verificador == 6){
+        num1 = 0
+        num1 += Number(visor.value)
+        visor.value = ' '
+    }else{
+        num1 += Number(visor.value)
+        visor.value = ' '
+    }
 
-    num1 = Number(visor.value)
+    if(verificador == 1 || verificador == 2 || verificador == 3 || verificador == 4 || verificador == 5){
+        num2 = num1 + Number(visor.value)
+
+        verificador = 10
+    }
+
+    verificador = 1
+}
+
+function ClicarMenos(){
+    if(verificador == 6){
+        num1 = 0
+        num1 += Number(visor.value)
+        visor.value = ' '
+    }else{
+        num1 += Number(visor.value)
+        visor.value = ' '
+    }
+
+    if(verificador == 1 || verificador == 2 || verificador == 3 || verificador == 4 || verificador == 5){
+        num2 = num1 - Number(visor.value)
+
+        verificador = 10
+    }
+
+    verificador = 2
+}
+
+function ClicarMulti(){
+    if(verificador == 6){
+        num1 = 0
+        num1 += Number(visor.value)
+        visor.value = ' '
+    }else{
+        num1 += Number(visor.value)
+        visor.value = ' '
+    }
+
+    if(verificador == 1 || verificador == 2 || verificador == 3 || verificador == 4 || verificador == 5){
+        num2 = num1 * Number(visor.value)
+
+        verificador = 10
+    }
+
+    verificador = 3
+}
+
+function ClicarDivi(){
+    if(verificador == 6){
+        num1 = 0
+        num1 += Number(visor.value)
+        visor.value = ' '
+    }else{
+        num1 += Number(visor.value)
+        visor.value = ' '
+    }
+
+    if(verificador == 1 || verificador == 2 || verificador == 3 || verificador == 4 || verificador == 5){
+        num2 = num1 / Number(visor.value)
+
+        verificador = 10
+    }
+
+    verificador = 4
+}
+
+function ClicarPorcento(){
+    if(verificador == 6){
+        num1 = 0
+        num1 += Number(visor.value)
+        visor.value = ' '
+    }else{
+        num1 += Number(visor.value)
+        visor.value = ' '
+    }
+
+    if(verificador == 1 || verificador == 2 || verificador == 3 || verificador == 4 || verificador == 5){
+        num2 = (Number(visor.value)*num1)/100
+
+        verificador = 10
+    }
+
+    verificador = 5
+}
+
+function ClicarIgual(){
+    let res = 0
+
+    if(verificador == 10){
+        if(verificador == 1){
+            res = num1 + num2
+            visor.value = res
+        }
+    
+        if(verificador == 2){
+            res = num1 - num2
+            visor.value = res
+        }
+    
+        if(verificador == 3){
+            res = num1 * num2
+            visor.value = res
+        }
+    
+        if(verificador == 4){
+            res = num1/num2
+            visor.value = res
+        }
+    
+        if(verificador == 5){
+            res = (num2*num1)/100 
+            visor.value = res
+        }
+    }else{
+    num2 = Number(visor.value)
+
+        if(verificador == 1){
+        res = num1 + num2
+        visor.value = res
+    }
+
+        if(verificador == 2){
+        res = num1 - num2
+        visor.value = res
+    }
+
+        if(verificador == 3){
+        res = num1 * num2
+        visor.value = res
+    }
+
+        if(verificador == 4){
+        res = num1/num2
+        visor.value = res
+    }
+
+        if(verificador == 5){
+        res = (num2*num1)/100 
+        visor.value = res
+    }
+}
+    verificador = 6
 }
