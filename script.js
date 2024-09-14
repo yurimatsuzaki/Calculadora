f54e605adf4cf71df23da3ba4de31640f5863e2f
yurimatsuzaki <155598762+yurimatsuzaki@users.noreply.github.com>
2024-09-14 17:10:42 -0300

    quase terminando o projeto

--- script.js
+++ script.js
@@ -1,100 +1,208 @@
 let visor = document.getElementById('visor')
-let num1 = ''
-let num2 = 0
-let num3 = ''
+let FistNumber = ''
+let SecundNumber = 0
 let verificador = 0
 
 function Clicar1(){
     visor.value += '1'
-    num1 += '1'
+    FistNumber += '1'
 }
 
 function Clicar2(){
     visor.value += '2'
-    num1 += '2'
+    FistNumber += '2'
 }
 
 function Clicar3(){
-    
+    visor.value += '3'
+    FistNumber += '3'
 }
 
 function Clicar4(){
-    
+    visor.value += '4'
+    FistNumber += '4'
 }
 
 function Clicar5(){
-    
+    visor.value += '5'
+    FistNumber += '5'
 }
 
 function Clicar6(){
-   
+    visor.value += '6'
+    FistNumber += '6'
 }
 
 function Clicar7(){
-  
+    visor.value += '7'
+    FistNumber += '7'
 }
 
 function Clicar8(){
-  
+    visor.value += '8'
+    FistNumber += '8'
 }
 
 function Clicar9(){
- 
+    visor.value += '9'
+    FistNumber += '9'
 }
 
 function ClicarZero(){
- 
+    visor.value += '0'
+    FistNumber += '0'
 }
 
 function ClicarLimpar(){
     visor.value = ' '
-    num1 = ''
-    num2 = 0
-    num3 = ''
+    FistNumber = ''
+    SecundNumber = 0
+    verificador = 0
 }
 
 function ClicarMais(){
-    visor.value += '+'
-    num2 += Number(num1)
-    num1 = ''
-
-    verificador = 1
+    if(verificador != 0){
+        alert('Você já colocou o operador!')
+    }else if(visor.value.length == 0 || visor.value == ' '){
+        alert('Adicione um número antes')
+    }else if(ClicarIgual() == true){
+        SecundNumber += visor.value
+        FistNumber = ''
+        visor.value += '+'
+        verificador = 1
+    }else{
+        SecundNumber += Number(FistNumber)
+        FistNumber = ''
+        visor.value += '+'
+        verificador = 1
+    }
 }
 
-function ClicarMenos(){
-    visor.value += '-'
-    
-    if(verificador == 1){
-        num2 = Number(num2) + Number(num1)
-        num1 = ''
-    }else if(verificador == 2){
-        num2 = Number(num2) - Number(num1)
-        num1 = ''
-    }else{
-        num2 += Number(num1)
-    }
 
-    verificador = 2
+function ClicarMenos(){
+    if(verificador != 0){
+        alert('Você já colocou o operador!')
+    }else if(visor.value.length == 0 || visor.value == ' '){
+        alert('Adicione um número antes')
+    }else if(ClicarIgual() == true){
+        SecundNumber += visor.value
+        FistNumber = ''
+        visor.value += '-'
+        verificador = 2
+    }else{
+        SecundNumber += Number(FistNumber)
+        FistNumber = ''
+        visor.value += '-'
+        verificador = 2
+    }
 }
 
 function ClicarMulti(){
- 
+    if(verificador != 0){
+        alert('Você já colocou o operador!')
+    }else if(visor.value.length == 0 || visor.value == ' '){
+        alert('Adicione um número antes')
+    }else if(ClicarIgual() == true){
+        SecundNumber += visor.value
+        FistNumber = ''
+        visor.value += 'x'
+        verificador = 3
+    }else{
+        SecundNumber += Number(FistNumber)
+        FistNumber = ''
+        visor.value += 'x'
+        verificador = 3
+    }
 }
 
 function ClicarDivi(){
-    
+    if(verificador != 0){
+        alert('Você já colocou o operador!')
+    }else if(visor.value.length == 0 || visor.value == ' '){
+        alert('Adicione um número antes')
+    }else if(ClicarIgual() == true){
+        SecundNumber += visor.value
+        FistNumber = ''
+        visor.value += '/'
+        verificador = 4
+    }else{
+        SecundNumber += Number(FistNumber)
+        FistNumber = ''
+        visor.value += '/'
+        verificador = 4
+    }
 }
 
 function ClicarPorcento(){
-  
+    if(verificador != 0){
+        alert('Você já colocou o operador!')
+    }else if(visor.value.length == 0 || visor.value == ' '){
+        alert('Adicione um número antes')
+    }else if(ClicarIgual() == true){
+        SecundNumber += visor.value
+        FistNumber = ''
+        visor.value += '%'
+        verificador = 5
+    }else{
+        SecundNumber += Number(FistNumber)
+        FistNumber = ''
+        visor.value += '%'
+        verificador = 5
+    }
+}
+
+function somar(num1, num2){
+   return Number(num1) + Number(num2)
+}
+
+function subtrair(num1, num2){
+   return Number(num1) - Number(num2)
+}
+
+function multiplicar(num1, num2){
+   return Number(num1) * Number(num2)
+}
+
+function dividir(num1, num2){
+   return Number(num1) / Number(num2)
+}
+
+function porcentgem(num1, num2){
+   return (Number(num1) * Number(num2))/100
 }
 
 function ClicarIgual(){
-    if(verificador == 1){
-        visor.value = Number(num2) + Number(num1)
+    if(verificador == 5){
+        visor.value = porcentgem(SecundNumber, FistNumber)
+        verificador = 0
+        FistNumber = 0
+        SecundNumber = 0
     }
 
-    if(verificador == 2){
-        visor.value = Number(num2) - Number(num1)
+    if(verificador == 3){
+        visor.value = multiplicar(SecundNumber, FistNumber)
+        verificador = 0
+        FistNumber = 0
+        SecundNumber = 0
     }
+    if(verificador == 4){
+        visor.value = dividir(SecundNumber, FistNumber)
+        verificador = 0
+        FistNumber = 0
+        SecundNumber = 0
+    }
+
+    if(verificador == 1){
+        visor.value = somar(SecundNumber, FistNumber)
+        verificador = 0
+        FistNumber = 0
+        SecundNumber = 0
+    }
+    if(verificador == 2){
+        visor.value = subtrair(SecundNumber, FistNumber)
+        verificador = 0
+        FistNumber = 0
+        SecundNumber = 0
+    }
+    return true
 }
