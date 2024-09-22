2e38b0429bdf841f690a3c0bb68a98ff9225f844
yurimatsuzaki <155598762+yurimatsuzaki@users.noreply.github.com>
2024-09-22 11:49:42 -0300

    documentando o código

--- script.js
+++ script.js
@@ -1,71 +1,71 @@
-let visor = document.getElementById('visor')
-let FistNumber = ''
-let SecundNumber = 0
-let verificador = 0
+let visor = document.getElementById('visor') // variável que pega o elemento input usado como visor
+let FistNumber = '' // variável usada para armazenar a sequência de números em formato de string
+let SecundNumber = 0 // variável que armazena o valor do visor em formato numérico para ser usado nas operações
+let verificador = 0 // variável usada como controlhe para tomadas de decisão dentro do código
 
-function Clicar1(){
+function Clicar1(){ // função para inserir 1 no visor e na variável 'FistNumber'
     visor.value += '1'
     FistNumber += '1'
 }
 
-function Clicar2(){
+function Clicar2(){ // função para inserir 2 no visor e na variável 'FistNumber'
     visor.value += '2'
     FistNumber += '2'
 }
 
-function Clicar3(){
+function Clicar3(){ // função para inserir 3 no visor e na variável 'FistNumber'
     visor.value += '3'
     FistNumber += '3'
 }
 
-function Clicar4(){
+function Clicar4(){ // função para inserir 4 no visor e na variável 'FistNumber'
     visor.value += '4'
     FistNumber += '4'
 }
 
-function Clicar5(){
+function Clicar5(){ // função para inserir 5 no visor e na variável 'FistNumber'
     visor.value += '5'
     FistNumber += '5'
 }
 
-function Clicar6(){
+function Clicar6(){ // função para inserir 6 no visor e na variável 'FistNumber'
     visor.value += '6'
     FistNumber += '6'
 }
 
-function Clicar7(){
+function Clicar7(){ // função para inserir 7 no visor e na variável 'FistNumber'
     visor.value += '7'
     FistNumber += '7'
 }
 
-function Clicar8(){
+function Clicar8(){ // função para inserir 8 no visor e na variável 'FistNumber'
     visor.value += '8'
     FistNumber += '8'
 }
 
-function Clicar9(){
+function Clicar9(){ // função para inserir 9 no visor e na variável 'FistNumber'
     visor.value += '9'
     FistNumber += '9'
 }
 
-function ClicarZero(){
+function ClicarZero(){ // função para inserir 0 no visor e na variável 'FistNumber'
     visor.value += '0'
     FistNumber += '0'
 }
 
-function ClicarPonto(){
+function ClicarPonto(){ // função para inserir '.' no visor e na variável 'FistNumber'
     visor.value += '.'
     FistNumber += '.'
 }
 
-function ClicarLimpar(){
+function ClicarLimpar(){ // função usada para limpar os valores do visor e das variáveis para voltar ao estágio inicial
     visor.value = ''
     FistNumber = ''
     SecundNumber = 0
     verificador = 0
 }
 
-function ClicarMais(){
+function ClicarMais(){ // função para inserir '+' no visor, alocar o valor de 'FistNumber'(já convertido em number) em 'SecundNumber' e tomar algumas medidas preventivas
     if(verificador != 0){
         alert('Você já colocou o operador!')
     }else if(visor.value.length == 0 || visor.value == ''){
@@ -83,8 +83,7 @@ function ClicarMais(){
     }
 }
 
-
-function ClicarMenos(){
+function ClicarMenos(){ // função para inserir '-' no visor, alocar o valor de 'FistNumber'(já convertido em number) em 'SecundNumber' e tomar algumas medidas preventivas
     if(verificador != 0){
         alert('Você já colocou o operador!')
     }else if(visor.value.length == 0 || visor.value == ' '){
@@ -102,7 +101,7 @@ function ClicarMenos(){
     }
 }
 
-function ClicarMulti(){
+function ClicarMulti(){ // função para inserir 'x' no visor, alocar o valor de 'FistNumber'(já convertido em number) em 'SecundNumber' e tomar algumas medidas preventivas
     if(verificador != 0){
         alert('Você já colocou o operador!')
     }else if(visor.value.length == 0 || visor.value == ' '){
@@ -120,7 +119,7 @@ function ClicarMulti(){
     }
 }
 
-function ClicarDivi(){
+function ClicarDivi(){ // função para inserir '/' no visor, alocar o valor de 'FistNumber'(já convertido em number) em 'SecundNumber' e tomar algumas medidas preventivas
     if(verificador != 0){
         alert('Você já colocou o operador!')
     }else if(visor.value.length == 0 || visor.value == ' '){
@@ -138,7 +137,7 @@ function ClicarDivi(){
     }
 }
 
-function ClicarPorcento(){
+function ClicarPorcento(){ // função para inserir '%' no visor, alocar o valor de 'FistNumber'(já convertido em number) em 'SecundNumber' e tomar algumas medidas preventivas
     if(verificador != 0){
         alert('Você já colocou o operador!')
     }else if(visor.value.length == 0 || visor.value == ' '){
@@ -156,27 +155,27 @@ function ClicarPorcento(){
     }
 }
 
-function somar(num1, num2){
+function somar(num1, num2){ // função para somar os valores digitados
    return Number(num1) + Number(num2)
 }
 
-function subtrair(num1, num2){
+function subtrair(num1, num2){ // função para subtrair os valores digitados
    return Number(num1) - Number(num2)
 }
 
-function multiplicar(num1, num2){
+function multiplicar(num1, num2){ // função para multiplicar os valores digitados
    return Number(num1) * Number(num2)
 }
 
-function dividir(num1, num2){
+function dividir(num1, num2){ // função para dividir os valores digitados
    return Number(num1) / Number(num2)
 }
 
-function porcentgem(num1, num2){
+function porcentgem(num1, num2){ // função para calcular a porcentagem do valor digitado
    return (Number(num1) * Number(num2))/100
 }
 
-function ClicarIgual(){
+function ClicarIgual(){ // função que imprime o resultado na tela a depender da operação escolhida
     if(verificador == 5){
         visor.value = porcentgem(SecundNumber, FistNumber)
         verificador = 0
